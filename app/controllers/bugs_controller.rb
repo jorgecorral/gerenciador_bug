2cf5068c3ec6a0baf482a8b6bff5a13a6978c481
JorgeCorral <jorgecorral@poli.ufrj.br>
2018-02-06 18:51:56 -0200

    Mensagens para slack configuradas

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -19,6 +19,7 @@ class BugsController < ApplicationController
 
   # GET /bugs/1/edit
   def edit
+
   end
 
   # POST /bugs
@@ -28,8 +29,14 @@ class BugsController < ApplicationController
 
     respond_to do |format|
       if @bug.save
-        format.html { redirect_to @bug, notice: 'Bug was successfully created.' }
+        format.html { redirect_to @bug, notice: 'Bug adicionado com sucesso!' }
         format.json { render :show, status: :created, location: @bug }
+    
+       notifier = Slack::Notifier.new "https://hooks.slack.com/services/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" do
+  defaults channel: "#general",
+         username: "notifier"
+        end
+        notifier.ping("Foi adicionado um bug no projeto " + @bug.projeto.nome + " cujo a descrição é " + @bug.descricao)
       else
         format.html { render :new }
         format.json { render json: @bug.errors, status: :unprocessable_entity }
@@ -42,8 +49,17 @@ class BugsController < ApplicationController
   def update
     respond_to do |format|
       if @bug.update(bug_params)
-        format.html { redirect_to @bug, notice: 'Bug was successfully updated.' }
+        format.html { redirect_to @bug, notice: 'Bug foi resolvido com sucesso!' }
         format.json { render :show, status: :ok, location: @bug }
+
+       if @bug.solucionado
+       notifier = Slack::Notifier.new "https://hooks.slack.com/services/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" do
+  defaults channel: "#general",
+         username: "notifier"
+        end
+        notifier.ping("O bug " + @bug.descricao + " foi solucionado - Projeto " + @bug.projeto.nome  + "\n A solução foi: "+ @bug.solucao)
+      end
+
       else
         format.html { render :edit }
         format.json { render json: @bug.errors, status: :unprocessable_entity }
@@ -56,7 +72,7 @@ class BugsController < ApplicationController
   def destroy
     @bug.destroy
     respond_to do |format|
-      format.html { redirect_to bugs_url, notice: 'Bug was successfully destroyed.' }
+      format.html { redirect_to bugs_url, notice: 'Bug deletado com sucesso!' }
       format.json { head :no_content }
     end
   end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+notifier = Slack::Notifier.new "https://hooks.slack.com/services/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" do
+  defaults channel: "#general",
+           username: "notifier"
+end
